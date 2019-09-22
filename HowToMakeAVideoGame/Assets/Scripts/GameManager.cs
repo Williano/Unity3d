85fc0ef08f8e5408b72b4855970533d072e35a59
Williano <paawilly17@gmail.com>
2019-09-22 21:14:26 +0000

    [NEW FEATURE] :sparkles: Add sound to game

--- HowToMakeAVideoGame/Assets/Scripts/GameManager.cs
+++ HowToMakeAVideoGame/Assets/Scripts/GameManager.cs
@@ -13,7 +13,6 @@ public class GameManager : MonoBehaviour
         if (!gameHasEnded)
         {
             gameHasEnded = true;
-            CompleteLevel();
             Invoke("RestartGame", restartDelay);
 
         }
