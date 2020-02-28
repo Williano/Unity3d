deda8fba5c9949c5da195074ecf0914fc4ec5852
Williano <paawilly17@gmail.com>
2020-02-28 03:24:52 -0600

    Add Collison to object

---
+++ ReviseUnityGame/Assets/Scripts/PlayerCollison.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollison : MonoBehaviour
+{
+    public PlayerMovement movement;
+    private void OnCollisionEnter(Collision collision)
+    {
+        movement.enabled = false;
+    }
+}
