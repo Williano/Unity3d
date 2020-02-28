571bf5e087fb5cf081385c26cbf56a092bdbc566
Williano <paawilly17@gmail.com>
2020-02-28 02:05:07 -0600

    Add first script

---
+++ ReviseUnityGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody Rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
